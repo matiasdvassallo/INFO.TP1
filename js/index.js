be2c04789163152c2504b1c3773c99121c7dec47
Matyty <2mdv2004@gmail.com>
2026-09-21 20:56:43 -0300

    nuevo index y css
    Empezando a armar lo que consideramos el index y su css definitivo

---
+++ js/index.js
@@ -0,0 +1,18 @@
+let juegos = document.querySelector('#juegosnav');
+let navdos = document.querySelector('#ul2');
+let menu = false
+
+juegos.addEventListener('click', (e) => {
+
+    e.preventDefault()
+    if (menu == false) {
+    navdos.innerHTML = '<li class="mainLi"><a href="juego1.html" target="_self">Juego 1</a></li>' +
+			            '<li class="mainLi"><a href="juego2.html" target="_self">Juego 2</a></li>' +
+						'<li class="mainLi"><a href="juego3.html" target="_self">Juego 3</a></li>';
+    menu = true;
+    } else {
+        navdos.innerHTML = '';
+        menu = false;
+    };
+
+});
